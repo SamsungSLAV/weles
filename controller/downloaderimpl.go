b31518fbe5b3c1cadc7cdb74578801d2d4115e31
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2017-11-20 06:04:31 +0100

    Add Downloader implementation with tests
    DownloaderImpl is responsible for parsing Job's config and searching
    for information about files (artifacts) related to the test.
    It delegates to ArtifactsManager downloading images and testfiles
    required to run Job (Push). It creates also paths for output
    artifacts (Pull). It updates Job's config with proper paths
    for these artifacts.
    
    It does also monitor process of downloading files in ArtifactManager
    and finally it notifies Controller when all files are downloaded
    or in case of failure.
    
    Change-Id: I578108dd1a924aaf36435e4dbc5d8dcce50214ee
    Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

---
+++ controller/downloaderimpl.go
@@ -0,0 +1,338 @@
+/*
+ *  Copyright (c) 2017-2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+// File controller/downloaderimpl.go contains Downloader implementation.
+
+package controller
+
+import (
+	"fmt"
+	"sync"
+
+	"git.tizen.org/tools/weles"
+	"git.tizen.org/tools/weles/controller/notifier"
+)
+
+const (
+	formatJobStatus = "Internal Weles error while changing Job status : %s"
+	formatJobConfig = "Internal Weles error while getting Job config : %s"
+	formatURI       = "Internal Weles error while registering URI:<%s> in ArtifactManager : %s"
+	formatPath      = "Internal Weles error while creating a new path in ArtifactManager : %s"
+	formatConfig    = "Internal Weles error while setting config : %s"
+	formatDownload  = "Failed to download some artifacts for the Job"
+	formatReady     = "%d / %d artifacts ready"
+)
+
+// jobArtifactsInfo contains information about progress of downloading
+// artifacts required by a single Job.
+type jobArtifactsInfo struct {
+	paths       int
+	ready       int
+	failed      int
+	configSaved bool
+}
+
+// DownloaderImpl implements delegating downloading of artifacts required
+// by Jobs to ArtifactsManager, monitors progress and notifies
+// Controller, when all files are ready.
+type DownloaderImpl struct {
+	// Notifier provides channel for communication with Controller.
+	notifier.Notifier
+	// jobs references module implementing Jobs management.
+	jobs JobsController
+	// artifacts references Weles module implementing ArtifactManager for
+	// managing ArtifactsDB.
+	artifacts weles.ArtifactManager
+	// collector gathers artifact status changes from ArtifactManager
+	collector chan weles.ArtifactStatusChange
+
+	// path2Job identifies Job related to the artifact path.
+	path2Job map[string]weles.JobID
+	// info contains information about progress of downloading artifacts
+	// for Jobs.
+	info map[weles.JobID]*jobArtifactsInfo
+	//mutex protects access to path2Job and info maps.
+	mutex *sync.Mutex
+}
+
+// NewDownloader creates a new DownloaderImpl structure setting up references
+// to used Weles modules.
+func NewDownloader(j JobsController, a weles.ArtifactManager) Downloader {
+	ret := &DownloaderImpl{
+		Notifier:  notifier.NewNotifier(),
+		jobs:      j,
+		artifacts: a,
+		collector: make(chan weles.ArtifactStatusChange),
+		path2Job:  make(map[string]weles.JobID),
+		info:      make(map[weles.JobID]*jobArtifactsInfo),
+		mutex:     new(sync.Mutex),
+	}
+	go ret.loop()
+	return ret
+}
+
+// pathStatusChange reacts on notification from ArtifactManager and updates
+// path and job structures.
+func (h *DownloaderImpl) pathStatusChange(path string, status weles.ArtifactStatus) (changed bool, j weles.JobID, info string) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+	j, ok := h.path2Job[path]
+	if !ok {
+		return
+	}
+	i, ok := h.info[j]
+	if !ok {
+		delete(h.path2Job, path)
+		return
+	}
+	switch status {
+	case weles.AM_READY:
+		i.ready++
+		info = fmt.Sprintf(formatReady, i.ready, i.paths)
+	case weles.AM_FAILED:
+		i.failed++
+		info = "Failed to download artifact"
+	default:
+		return
+	}
+	delete(h.path2Job, path)
+	changed = true
+	return
+}
+
+// removePath removes mapping from the path to related Job.
+func (h *DownloaderImpl) removePath(path string) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+	delete(h.path2Job, path)
+}
+
+// loop handles all notifications from ArtifactManager, updates
+// jobArtifactsInfo and send notification to Controller, when the answer
+// is ready.
+//
+// It is run in a separate goroutine - one for all jobs.
+func (h *DownloaderImpl) loop() {
+	for {
+		change, open := <-h.collector
+		if !open {
+			return
+		}
+		update, j, info := h.pathStatusChange(string(change.Path), change.NewStatus)
+		if !update {
+			continue
+		}
+
+		err := h.jobs.SetStatusAndInfo(j, weles.JOB_DOWNLOADING, info)
+		if err != nil {
+			h.removePath(string(change.Path))
+			h.fail(j, fmt.Sprintf(formatJobStatus, err.Error()))
+		}
+		h.sendIfReady(j)
+	}
+}
+
+// fail responses failure to Controller.
+func (h *DownloaderImpl) fail(j weles.JobID, msg string) {
+	if h.removeJobInfo(j) == nil {
+		h.SendFail(j, msg)
+	}
+}
+
+// succeed responses success to Controller.
+func (h *DownloaderImpl) succeed(j weles.JobID) {
+	if h.removeJobInfo(j) == nil {
+		h.SendOK(j)
+	}
+}
+
+// initializeJobInfo creates a jobArtifactInfo structure.
+func (h *DownloaderImpl) initializeJobInfo(j weles.JobID) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+	_, ok := h.info[j]
+	if !ok {
+		h.info[j] = new(jobArtifactsInfo)
+	}
+}
+
+// removeJobInfo removes a jobArtifactInfo structure.
+func (h *DownloaderImpl) removeJobInfo(j weles.JobID) error {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	_, ok := h.info[j]
+	if !ok {
+		return weles.ErrJobNotFound
+	}
+	delete(h.info, j)
+	return nil
+}
+
+// push delegates downloading single uri to ArtifactDB.
+func (h *DownloaderImpl) push(j weles.JobID, t weles.ArtifactType, alias string, uri string) (string, error) {
+	p, err := h.artifacts.PushArtifact(weles.ArtifactDescription{
+		JobID: j,
+		Type:  t,
+		Alias: weles.ArtifactAlias(alias),
+		URI:   weles.ArtifactURI(uri),
+	}, h.collector)
+	if err != nil {
+		return "", err
+	}
+
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	i, ok := h.info[j]
+	if !ok {
+		return "", weles.ErrJobNotFound
+	}
+	i.paths++
+	h.path2Job[string(p)] = j
+
+	return string(p), nil
+}
+
+// pullCreate creates a new path for pull artifact.
+func (h *DownloaderImpl) pullCreate(j weles.JobID, alias string) (string, error) {
+	p, err := h.artifacts.CreateArtifact(weles.ArtifactDescription{
+		JobID: j,
+		Type:  weles.AM_TESTFILE,
+		Alias: weles.ArtifactAlias(alias),
+	})
+	return string(p), err
+}
+
+// configSaved updates info structure.
+func (h *DownloaderImpl) configSaved(j weles.JobID) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	i, ok := h.info[j]
+	if !ok {
+		return
+	}
+
+	i.configSaved = true
+}
+
+// verify if an answer to the Controller should be send.
+func (h *DownloaderImpl) verify(j weles.JobID) (success, send bool) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	i, ok := h.info[j]
+	if !ok { // Job is not monitored (maybe it has been responded already).
+		return false, false
+	}
+	if i.failed > 0 { // Some artifacts fail to be downloaded.
+		return false, true
+	}
+	if !i.configSaved { // Config is not yet fully analyzed and saved.
+		return false, false
+	}
+	if i.ready == i.paths { // All artifacts are ready.
+		return true, true
+	}
+	return false, false
+}
+
+// sendIfReady sends an answer to the Controller if it is ready.
+func (h *DownloaderImpl) sendIfReady(j weles.JobID) {
+	success, send := h.verify(j)
+
+	if !send {
+		return
+	}
+
+	if success {
+		h.succeed(j)
+	} else {
+		h.fail(j, formatDownload)
+	}
+}
+
+// DispatchDownloads parses Job's config and delegates to ArtifactManager downloading
+// of all images and files to be pushed during Job execution. It also creates
+// ArtifactDB paths for files that will be pulled from Dryad.
+func (h *DownloaderImpl) DispatchDownloads(j weles.JobID) {
+	h.initializeJobInfo(j)
+
+	err := h.jobs.SetStatusAndInfo(j, weles.JOB_DOWNLOADING, "")
+	if err != nil {
+		h.fail(j, fmt.Sprintf(formatJobStatus, err.Error()))
+		return
+	}
+
+	config, err := h.jobs.GetConfig(j)
+	if err != nil {
+		h.fail(j, fmt.Sprintf(formatJobConfig, err.Error()))
+		return
+	}
+
+	for i, image := range config.Action.Deploy.Images {
+		if image.URI != "" {
+			path, err := h.push(j, weles.AM_IMAGEFILE, fmt.Sprintf("Image_%d", i), image.URI)
+			if err != nil {
+				h.fail(j, fmt.Sprintf(formatURI, image.URI, err.Error()))
+				return
+			}
+			config.Action.Deploy.Images[i].Path = path
+		}
+		if image.ChecksumURI != "" {
+			path, err := h.push(j, weles.AM_IMAGEFILE, fmt.Sprintf("ImageMD5_%d", i), image.ChecksumURI)
+			if err != nil {
+				h.fail(j, fmt.Sprintf(formatURI, image.ChecksumURI, err.Error()))
+				return
+			}
+			config.Action.Deploy.Images[i].ChecksumPath = path
+		}
+	}
+	for i, tc := range config.Action.Test.TestCases {
+		for k, ta := range tc.TestActions {
+			switch ta.(type) {
+			case weles.Push:
+				action := ta.(weles.Push)
+				path, err := h.push(j, weles.AM_TESTFILE, action.Alias, action.URI)
+				if err != nil {
+					h.fail(j, fmt.Sprintf(formatURI, action.URI, err.Error()))
+					return
+				}
+				action.Path = path
+				config.Action.Test.TestCases[i].TestActions[k] = action
+			case weles.Pull:
+				action := ta.(weles.Pull)
+				path, err := h.pullCreate(j, action.Alias)
+				if err != nil {
+					h.fail(j, fmt.Sprintf(formatPath, err.Error()))
+					return
+				}
+				action.Path = path
+				config.Action.Test.TestCases[i].TestActions[k] = action
+			}
+		}
+	}
+
+	err = h.jobs.SetConfig(j, config)
+	if err != nil {
+		h.fail(j, fmt.Sprintf(formatConfig, err.Error()))
+		return
+	}
+
+	h.configSaved(j)
+	h.sendIfReady(j)
+}
