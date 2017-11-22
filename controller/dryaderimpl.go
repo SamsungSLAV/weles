8ecfbfc18024bbf2464b8a5c8b9accaac31d6274
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2017-11-22 09:53:17 +0100

    Add Dryader implementation with tests
    DryaderImpl implements delegation and control of Job execution
    in DryadJobManager.
    
    Change-Id: I7d30f21bf0f838829f6c2d0b65362a6d7b66d0a3
    Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

---
+++ controller/dryaderimpl.go
@@ -0,0 +1,165 @@
+/*
+ *  Copyright (c) 2017-2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+// File controller/dryaderimpl.go implements Dryader interface. It delegates
+// and controls Job execution by DryadJobManager.
+
+package controller
+
+import (
+	"fmt"
+	"sync"
+
+	"git.tizen.org/tools/weles"
+	"git.tizen.org/tools/weles/controller/notifier"
+)
+
+// DryaderImpl implements Dryader. It delegates and controls Job execution
+// by DryadJobManager.
+type DryaderImpl struct {
+	// Notifier provides channel for communication with Controller.
+	notifier.Notifier
+	// jobs references module implementing Jobs management.
+	jobs JobsController
+	// djm manages DryadJobs.
+	djm weles.DryadJobManager
+	// info contains Jobs delegated to DryadJobManager and not completed yet
+	// - active Jobs collection.
+	info map[weles.JobID]bool
+	// mutex protects access to info map.
+	mutex *sync.Mutex
+	// listener listens on notifications from DryadJobManager.
+	listener chan weles.DryadJobStatusChange
+	// finish is channel for stopping internal goroutine.
+	finish chan int
+	// looper waits for internal goroutine running loop to finish.
+	looper sync.WaitGroup
+}
+
+// NewDryader creates a new DryaderImpl structure setting up references
+// to used Weles modules.
+func NewDryader(j JobsController, d weles.DryadJobManager) Dryader {
+	ret := &DryaderImpl{
+		Notifier: notifier.NewNotifier(),
+		jobs:     j,
+		djm:      d,
+		info:     make(map[weles.JobID]bool),
+		mutex:    new(sync.Mutex),
+		listener: make(chan weles.DryadJobStatusChange),
+		finish:   make(chan int),
+	}
+	ret.looper.Add(1)
+	go ret.loop()
+	return ret
+}
+
+// Finish internal goroutine.
+func (h *DryaderImpl) Finish() {
+	h.finish <- 1
+	h.looper.Wait()
+}
+
+// add adds a new Job delegated to DryadJobManager to active Jobs collection.
+func (h *DryaderImpl) add(j weles.JobID) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	h.info[j] = true
+}
+
+// remove Job from active Jobs collection.
+func (h *DryaderImpl) remove(j weles.JobID) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	delete(h.info, j)
+}
+
+// setStatus sets Jobs status to RUNNING and updates info.
+func (h *DryaderImpl) setStatus(j weles.JobID, msg string) {
+	err := h.jobs.SetStatusAndInfo(j, weles.JOB_RUNNING, msg)
+	if err != nil {
+		h.remove(j)
+		h.SendFail(j, fmt.Sprintf("Internal Weles error while changing Job status : %s", err.Error()))
+	}
+}
+
+// loop monitors DryadJob's status.
+func (h *DryaderImpl) loop() {
+	defer h.looper.Done()
+	for {
+		select {
+		case <-h.finish:
+			return
+		case recv := <-h.listener:
+			change := weles.DryadJobInfo(recv)
+			h.mutex.Lock()
+			_, ok := h.info[change.Job]
+			h.mutex.Unlock()
+			if !ok {
+				continue
+			}
+
+			switch change.Status {
+			case weles.DJ_NEW:
+				h.setStatus(change.Job, "Started")
+			case weles.DJ_DEPLOY:
+				h.setStatus(change.Job, "Deploying")
+			case weles.DJ_BOOT:
+				h.setStatus(change.Job, "Booting")
+			case weles.DJ_TEST:
+				h.setStatus(change.Job, "Testing")
+			case weles.DJ_FAIL:
+				h.remove(change.Job)
+				h.SendFail(change.Job, "Failed to execute test on Dryad.")
+			case weles.DJ_OK:
+				h.remove(change.Job)
+				h.SendOK(change.Job)
+			}
+		}
+	}
+}
+
+// StartJob registers new Job to be executed in DryadJobManager.
+func (h *DryaderImpl) StartJob(j weles.JobID) {
+	d, err := h.jobs.GetDryad(j)
+	if err != nil {
+		h.SendFail(j, fmt.Sprintf("Internal Weles error while getting Dryad for Job : %s", err.Error()))
+		return
+	}
+
+	h.add(j)
+
+	err = h.djm.Create(j, d, h.listener)
+	if err != nil {
+		h.remove(j)
+		h.SendFail(j, fmt.Sprintf("Cannot delegate Job to Dryad : %s", err.Error()))
+		return
+	}
+}
+
+// CancelJob breaks Job execution in DryadJobManager.
+func (h *DryaderImpl) CancelJob(j weles.JobID) {
+	h.mutex.Lock()
+	_, ok := h.info[j]
+	h.mutex.Unlock()
+	if !ok {
+		return
+	}
+
+	h.remove(j)
+	h.djm.Cancel(j)
+}
---
+++ controller/dryaderimpl_test.go
@@ -0,0 +1,226 @@
+/*
+ *  Copyright (c) 2017-2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+package controller
+
+import (
+	"errors"
+	"net"
+
+	"git.tizen.org/tools/weles"
+	cmock "git.tizen.org/tools/weles/controller/mock"
+	"git.tizen.org/tools/weles/controller/notifier"
+	mock "git.tizen.org/tools/weles/mock"
+	gomock "github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("DryaderImpl", func() {
+	var r <-chan notifier.Notification
+	var jc *cmock.MockJobsController
+	var djm *mock.MockDryadJobManager
+	var h Dryader
+	var ctrl *gomock.Controller
+	j := weles.JobID(0xCAFE)
+	dryad := weles.Dryad{Addr: &net.IPNet{IP: net.IPv4(1, 2, 3, 4), Mask: net.IPv4Mask(5, 6, 7, 8)}}
+	err := errors.New("test error")
+
+	expectRegistered := func(offset int) {
+		h.(*DryaderImpl).mutex.Lock()
+		defer h.(*DryaderImpl).mutex.Unlock()
+
+		ExpectWithOffset(offset, len(h.(*DryaderImpl).info)).To(Equal(1))
+		info, ok := h.(*DryaderImpl).info[j]
+		ExpectWithOffset(offset, ok).To(BeTrue())
+		ExpectWithOffset(offset, info).To(BeTrue())
+	}
+	eventuallyEmpty := func(offset int) {
+		EventuallyWithOffset(offset, func() int {
+			h.(*DryaderImpl).mutex.Lock()
+			defer h.(*DryaderImpl).mutex.Unlock()
+			return len(h.(*DryaderImpl).info)
+		}).Should(BeZero())
+	}
+	eventuallyNoti := func(offset int, ok bool, msg string) {
+		expectedNotification := notifier.Notification{
+			JobID: j,
+			OK:    ok,
+			Msg:   msg,
+		}
+		EventuallyWithOffset(offset, r).Should(Receive(Equal(expectedNotification)))
+	}
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+
+		jc = cmock.NewMockJobsController(ctrl)
+		djm = mock.NewMockDryadJobManager(ctrl)
+
+		h = NewDryader(jc, djm)
+		r = h.Listen()
+	})
+	AfterEach(func() {
+		h.(*DryaderImpl).Finish()
+		ctrl.Finish()
+	})
+
+	Describe("NewBoruter", func() {
+		It("should create a new object", func() {
+			Expect(h).NotTo(BeNil())
+			Expect(h.(*DryaderImpl).jobs).To(Equal(jc))
+			Expect(h.(*DryaderImpl).djm).To(Equal(djm))
+			Expect(h.(*DryaderImpl).info).NotTo(BeNil())
+			Expect(h.(*DryaderImpl).mutex).NotTo(BeNil())
+			Expect(h.(*DryaderImpl).finish).NotTo(BeNil())
+		})
+	})
+
+	Describe("StartJob", func() {
+		It("should register job successfully", func() {
+			jc.EXPECT().GetDryad(j).Return(dryad, nil)
+			djm.EXPECT().Create(j, dryad, (chan<- weles.DryadJobStatusChange)(h.(*DryaderImpl).listener))
+
+			h.StartJob(j)
+			expectRegistered(1)
+		})
+		It("should fail if DryadJobManager.Create fails", func() {
+			jc.EXPECT().GetDryad(j).Return(dryad, nil)
+			djm.EXPECT().Create(j, dryad, (chan<- weles.DryadJobStatusChange)(h.(*DryaderImpl).listener)).Return(err)
+
+			h.StartJob(j)
+
+			eventuallyNoti(1, false, "Cannot delegate Job to Dryad : test error")
+			eventuallyEmpty(1)
+		})
+		It("should fail if JobManager.GetDryad fails", func() {
+			jc.EXPECT().GetDryad(j).Return(weles.Dryad{}, err)
+
+			h.StartJob(j)
+
+			eventuallyNoti(1, false, "Internal Weles error while getting Dryad for Job : test error")
+			eventuallyEmpty(1)
+		})
+	})
+
+	Describe("With registered request", func() {
+		updateStates := []weles.DryadJobStatus{
+			weles.DJ_NEW,
+			weles.DJ_DEPLOY,
+			weles.DJ_BOOT,
+			weles.DJ_TEST,
+		}
+		updateMsgs := []string{
+			"Started",
+			"Deploying",
+			"Booting",
+			"Testing",
+		}
+		BeforeEach(func() {
+			jc.EXPECT().GetDryad(j).Return(dryad, nil)
+			djm.EXPECT().Create(j, dryad, (chan<- weles.DryadJobStatusChange)(h.(*DryaderImpl).listener))
+
+			h.StartJob(j)
+
+			expectRegistered(1)
+		})
+
+		It("should ignore ID of not registered request", func() {
+			states := []weles.DryadJobStatus{
+				weles.DJ_NEW,
+				weles.DJ_DEPLOY,
+				weles.DJ_BOOT,
+				weles.DJ_TEST,
+				weles.DJ_FAIL,
+				weles.DJ_OK,
+			}
+			for _, s := range states {
+				change := weles.DryadJobInfo{Job: weles.JobID(0x0BCA), Status: s}
+				h.(*DryaderImpl).listener <- weles.DryadJobStatusChange(change)
+
+				expectRegistered(1)
+			}
+		})
+		It("should update status of the Job", func() {
+			for i, s := range updateStates {
+				change := weles.DryadJobInfo{Job: j, Status: s}
+				jc.EXPECT().SetStatusAndInfo(j, weles.JOB_RUNNING, updateMsgs[i])
+
+				h.(*DryaderImpl).listener <- weles.DryadJobStatusChange(change)
+
+				expectRegistered(1)
+			}
+		})
+		updateTableEntries := func() []TableEntry {
+			var ret []TableEntry
+			for i, s := range updateStates {
+				ret = append(ret, Entry(string(s), s, updateMsgs[i]))
+			}
+			return ret
+		}()
+		DescribeTable("should fail if updating status of the Job fails",
+			func(s weles.DryadJobStatus, msg string) {
+				change := weles.DryadJobInfo{Job: j, Status: s}
+				jc.EXPECT().SetStatusAndInfo(j, weles.JOB_RUNNING, msg).Return(err)
+
+				h.(*DryaderImpl).listener <- weles.DryadJobStatusChange(change)
+
+				eventuallyNoti(1, false, "Internal Weles error while changing Job status : test error")
+				eventuallyEmpty(1)
+			},
+			updateTableEntries...,
+		)
+
+		It("should fail if Dryad Job fails", func() {
+			change := weles.DryadJobInfo{Job: j, Status: weles.DJ_FAIL}
+
+			h.(*DryaderImpl).listener <- weles.DryadJobStatusChange(change)
+
+			eventuallyNoti(1, false, "Failed to execute test on Dryad.")
+			eventuallyEmpty(1)
+		})
+		It("should notify about successfully completed Dryad Job", func() {
+			change := weles.DryadJobInfo{Job: j, Status: weles.DJ_OK}
+
+			h.(*DryaderImpl).listener <- weles.DryadJobStatusChange(change)
+
+			eventuallyNoti(1, true, "")
+			eventuallyEmpty(1)
+		})
+
+		Describe("CancelJob", func() {
+			It("should remove Job and cancel it in Dryad Job Manager", func() {
+				djm.EXPECT().Cancel(j)
+
+				h.CancelJob(j)
+
+				eventuallyEmpty(1)
+			})
+			It("should ignore djm's Cancel error", func() {
+				djm.EXPECT().Cancel(j).Return(err)
+
+				h.CancelJob(j)
+
+				eventuallyEmpty(1)
+			})
+			It("should ignore not existing request", func() {
+				h.CancelJob(weles.JobID(0x0BCA))
+				expectRegistered(1)
+			})
+		})
+	})
+})
