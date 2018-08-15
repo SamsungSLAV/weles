4c2c6886cebdf8acbca4e1165e7ec8bc793eeb5d
Alexander Mazuruk <a.mazuruk@samsung.com>
2018-08-15 14:41:49 +0200

    Adjust database tests
    Make artifacts/database tests faster (8s->2.5s) by restructurizing
    before/afterEach clauses to omit unneccessary setup before TCs.
    Finished packing setups into single transactions with db.
    
    Merged tests for pagination into database tests.
    
    Add tests for sorting
    
    Less magic
    
    Change-Id: I49c82c4d0bfc000498b555c0d56490d939e24713
    Signed-off-by: Alexander Mazuruk <a.mazuruk@samsung.com>

--- artifacts/database/database_test.go
+++ artifacts/database/database_test.go
@@ -40,8 +40,6 @@ var _ = Describe("ArtifactDB", func() {
 		invalidStatus weles.ArtifactStatus = "invalidStatus"
 		invalidType   weles.ArtifactType   = "invalidType"
 		invalidAlias  weles.ArtifactAlias  = "invalidAlias"
-		goldenUnicorn ArtifactDB
-		tmpDir        string
 
 		artifact = weles.ArtifactInfo{
 			ArtifactDescription: weles.ArtifactDescription{
@@ -174,180 +172,302 @@ var _ = Describe("ArtifactDB", func() {
 			Alias:  nil,
 		}
 		emptyFilter = weles.ArtifactFilter{}
-		//default values of sorter passed by server
-		defaultSorter = weles.ArtifactSorter{
+
+		emptyPaginator = weles.ArtifactPagination{}
+
+		descendingSorter = weles.ArtifactSorter{
 			SortOrder: weles.SortOrderDescending,
 			SortBy:    weles.ArtifactSortByID,
 		}
-	)
 
-	jobsInDB := func(job weles.JobID) int64 {
-		n, err := goldenUnicorn.dbmap.SelectInt(`SELECT COUNT(*)
- 		FROM artifacts
- 		WHERE JobID = ?`, job)
+		defaultSorter = descendingSorter
+
+		ascendingSorter = weles.ArtifactSorter{
+			SortOrder: weles.SortOrderAscending,
+			SortBy:    weles.ArtifactSortByID,
+		}
+	)
+	jobInDB := func(job weles.JobID, db ArtifactDB) bool {
+		n, err := db.dbmap.SelectInt(
+			`SELECT COUNT(*)
+ 			FROM artifacts
+ 			WHERE JobID = ?`, job)
 		Expect(err).ToNot(HaveOccurred())
-		return n
+		return bool(n > 0)
 	}
 
-	BeforeEach(func() {
-		var err error
-		tmpDir, err = ioutil.TempDir("", "weles-")
-		Expect(err).ToNot(HaveOccurred())
-		err = goldenUnicorn.Open(filepath.Join(tmpDir, "test.db"))
-		Expect(err).ToNot(HaveOccurred())
-	})
+	Describe("Not pagination", func() {
 
-	AfterEach(func() {
-		err := goldenUnicorn.Close()
-		Expect(err).ToNot(HaveOccurred())
-		err = os.RemoveAll(tmpDir)
-		Expect(err).ToNot(HaveOccurred())
-	})
-
-	It("should open new database, with artifact table", func() {
-		n, err := goldenUnicorn.dbmap.SelectInt(`SELECT COUNT(*)
- 		FROM sqlite_master
- 		WHERE name = 'artifacts'
- 		AND type = 'table'`)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(n).To(BeNumerically("==", 1))
-	})
-
-	It("should fail to open database on invalid path", func() {
-		// sql.Open only validates arguments.
-		// db.Ping must be called to check the connection.
-		invalidDatabasePath := filepath.Join(tmpDir, "invalid", "test.db")
-		err := goldenUnicorn.Open(invalidDatabasePath)
-		Expect(err).To(HaveOccurred())
-		Expect(invalidDatabasePath).ToNot(BeAnExistingFile())
-	})
-
-	It("should insert new artifact to database", func() {
-		Expect(jobsInDB(job)).To(BeNumerically("==", 0))
-
-		err := goldenUnicorn.InsertArtifactInfo(&artifact)
-		Expect(err).ToNot(HaveOccurred())
-
-		Expect(jobsInDB(artifact.JobID)).To(BeNumerically("==", 1))
-	})
-
-	Describe("SelectPath", func() {
-
+		var (
+			goldenUnicorn ArtifactDB
+			tmpDir        string
+		)
 		BeforeEach(func() {
+			var err error
+			tmpDir, err = ioutil.TempDir("", tmpDirPrefix)
+			Expect(err).ToNot(HaveOccurred())
+			err = goldenUnicorn.Open(filepath.Join(tmpDir, "test.db"))
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			err := goldenUnicorn.Close()
+			Expect(err).ToNot(HaveOccurred())
+			err = os.RemoveAll(tmpDir)
+			Expect(err).ToNot(HaveOccurred())
+		})
+		It("should open new database, with artifact table", func() {
+			n, err := goldenUnicorn.dbmap.SelectInt(
+				`SELECT COUNT(*)
+ 				FROM sqlite_master
+ 				WHERE name = 'artifacts'
+ 				AND type = 'table'`)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(n).To(BeNumerically("==", 1))
+		})
+
+		It("should fail to open database on invalid path", func() {
+			// sql.Open only validates arguments.
+			// db.Ping must be called to check the connection.
+			invalidDatabasePath := filepath.Join(tmpDir, "invalid", "test.db")
+			err := goldenUnicorn.Open(invalidDatabasePath)
+			Expect(err).To(HaveOccurred())
+			Expect(invalidDatabasePath).ToNot(BeAnExistingFile())
+		})
+
+		It("should insert new artifact to database", func() {
+			Expect(jobInDB(job, goldenUnicorn)).To(BeFalse())
+
 			err := goldenUnicorn.InsertArtifactInfo(&artifact)
 			Expect(err).ToNot(HaveOccurred())
 
-			Expect(jobsInDB(artifact.JobID)).To(BeNumerically("==", 1))
+			Expect(jobInDB(artifact.JobID, goldenUnicorn)).To(BeTrue())
 		})
 
-		DescribeTable("database selectpath",
-			func(path weles.ArtifactPath, expectedErr error, expectedArtifact weles.ArtifactInfo) {
-				result, err := goldenUnicorn.SelectPath(path)
-
-				if expectedErr != nil {
-					Expect(err).To(Equal(expectedErr))
-				} else {
+		Describe("SetStatus", func() {
+			BeforeEach(func() {
+				trans, err := goldenUnicorn.dbmap.Begin()
+				Expect(err).ToNot(HaveOccurred())
+				defer trans.Commit()
+				for _, a := range testArtifacts {
+					err := trans.Insert(&a)
 					Expect(err).ToNot(HaveOccurred())
 				}
-				expectedArtifact.ID = result.ID
-				Expect(result).To(Equal(expectedArtifact))
-			},
-			Entry("retrieve artifact based on path",
-				artifact.Path, nil, artifact),
-			Entry("retrieve artifact based on invalid path",
-				invalidPath, sql.ErrNoRows, weles.ArtifactInfo{}),
-		)
-	})
-
-	Describe("List", func() {
-		BeforeEach(func() {
-			trans, err := goldenUnicorn.dbmap.Begin()
-			Expect(err).ToNot(HaveOccurred())
-			defer trans.Commit()
-			for _, a := range testArtifacts {
-				err := trans.Insert(&a)
-				Expect(err).ToNot(HaveOccurred())
-			}
+			})
+			DescribeTable("artifact status change",
+				func(change weles.ArtifactStatusChange, expectedErr error) {
+					var a weles.ArtifactInfo
+					err := goldenUnicorn.SetStatus(change)
+					if expectedErr == nil {
+						Expect(err).ToNot(HaveOccurred())
+						a, err = goldenUnicorn.SelectPath(change.Path)
+						Expect(err).ToNot(HaveOccurred())
+						Expect(a.Status).To(Equal(change.NewStatus))
+					} else {
+						Expect(err).To(Equal(expectedErr))
+						a, err = goldenUnicorn.SelectPath(change.Path)
+						Expect(err).To(HaveOccurred())
+						Expect(a).To(Equal(weles.ArtifactInfo{}))
+					}
+				},
+				Entry("change status of artifact not present in ArtifactDB",
+					weles.ArtifactStatusChange{
+						Path:      invalidPath,
+						NewStatus: weles.ArtifactStatusDOWNLOADING,
+					},
+					sql.ErrNoRows),
+				Entry("change status of artifact present in ArtifactDB",
+					weles.ArtifactStatusChange{
+						Path:      artifact.Path,
+						NewStatus: weles.ArtifactStatusDOWNLOADING,
+					},
+					nil),
+			)
 		})
-		DescribeTable("list artifacts matching filter",
-			func(filter weles.ArtifactFilter, expected ...weles.ArtifactInfo) {
-				results, _, err := goldenUnicorn.Filter(filter, defaultSorter,
-					weles.ArtifactPagination{})
+
+		Describe("SelectPath", func() {
+
+			BeforeEach(func() {
+				err := goldenUnicorn.InsertArtifactInfo(&artifact)
 				Expect(err).ToNot(HaveOccurred())
-				//TODO: match all fields except ID.
-				for i := range results {
-					for j := range expected {
-						if results[i].JobID == expected[j].JobID {
-							if results[i].URI == expected[j].URI {
-								expected[j].ID = results[i].ID
+
+				Expect(jobInDB(artifact.JobID, goldenUnicorn)).To(BeTrue())
+			})
+
+			DescribeTable("database selectpath",
+				func(path weles.ArtifactPath, expectedErr error,
+					expectedArtifact weles.ArtifactInfo) {
+					result, err := goldenUnicorn.SelectPath(path)
+
+					if expectedErr != nil {
+						Expect(err).To(Equal(expectedErr))
+					} else {
+						Expect(err).ToNot(HaveOccurred())
+					}
+					expectedArtifact.ID = result.ID
+					Expect(result).To(Equal(expectedArtifact))
+				},
+				Entry("retrieve artifact based on path", artifact.Path, nil, artifact),
+				Entry("retrieve artifact based on invalid path", invalidPath, sql.ErrNoRows,
+					weles.ArtifactInfo{}),
+			)
+		})
+
+		Describe("List", func() {
+			BeforeEach(func() {
+				trans, err := goldenUnicorn.dbmap.Begin()
+				Expect(err).ToNot(HaveOccurred())
+				defer trans.Commit()
+				for _, a := range testArtifacts {
+					err := trans.Insert(&a)
+					Expect(err).ToNot(HaveOccurred())
+				}
+			})
+			DescribeTable("list artifacts matching filter",
+				func(filter weles.ArtifactFilter, expected ...weles.ArtifactInfo) {
+					results, _, err := goldenUnicorn.Filter(filter, defaultSorter, emptyPaginator)
+					Expect(err).ToNot(HaveOccurred())
+					//TODO: match all fields except ID.
+					for i := range results {
+						for j := range expected {
+							if results[i].JobID == expected[j].JobID {
+								if results[i].URI == expected[j].URI {
+									expected[j].ID = results[i].ID
+								}
 							}
 						}
 					}
+					Expect(results).To(ConsistOf(expected))
+				},
+				Entry("filter one JobID", oneJobFilter, artifact),
+				Entry("filter more than one JobIDs", twoJobsFilter, artifact, aImageReady,
+					aYamlFailed),
+				Entry("filter one Type", oneTypeFilter, aYamlFailed),
+				Entry("filter more than one Type", twoTypesFilter, aYamlFailed, aTestFailed),
+				Entry("filter one Status", oneStatusFilter, artifact),
+				Entry("filter more than one Status", twoStatusFilter, artifact, aTestFailed,
+					aYamlFailed),
+				Entry("filter one Alias", oneAliasFilter, artifact),
+				Entry("filter more than one Alias", twoAliasFilter, artifact, aImageReady,
+					aYamlFailed),
+				Entry("filter is completly set up", fullFilter, aYamlFailed),
+				Entry("filter is empty", emptyFilter, artifact, aImageReady, aYamlFailed,
+					aTestFailed),
+			)
 
-				}
-				Expect(results).To(ConsistOf(expected))
-			},
-			Entry("filter one JobID", oneJobFilter, artifact),
-			Entry("filter more than one JobIDs", twoJobsFilter, artifact, aImageReady, aYamlFailed),
-			Entry("filter one Type", oneTypeFilter, aYamlFailed),
-			Entry("filter more than one Type", twoTypesFilter, aYamlFailed, aTestFailed),
-			Entry("filter one Status", oneStatusFilter, artifact),
-			Entry("filter more than one Status", twoStatusFilter, artifact, aTestFailed,
-				aYamlFailed),
-			Entry("filter one Alias", oneAliasFilter, artifact),
-			Entry("filter more than one Alias", twoAliasFilter, artifact, aImageReady, aYamlFailed),
-			Entry("filter is completly set up", fullFilter, aYamlFailed),
-			Entry("filter is empty", emptyFilter, artifact, aImageReady, aYamlFailed, aTestFailed),
-		)
-
-		DescribeTable("return artifact not found error",
-			func(filter weles.ArtifactFilter, expected ...weles.ArtifactInfo) {
-				_, _, err := goldenUnicorn.Filter(filter, defaultSorter, weles.ArtifactPagination{})
-				Expect(err).To(Equal(weles.ErrArtifactNotFound))
-			},
-			Entry("filter JobID not in db", noJobFilter),
-			Entry("filter Type not in db", noTypeFilter),
-			Entry("filter Status not in db", noStatusFilter),
-			Entry("filter Alias not in db", noAliasFilter),
-			Entry("no artifact in db matches filter", noMatchFilter),
-		)
-	})
-	Describe("SetStatus", func() {
-		BeforeEach(func() {
-			for _, a := range testArtifacts {
-				err := goldenUnicorn.InsertArtifactInfo(&a)
-				Expect(err).ToNot(HaveOccurred())
-			}
+			DescribeTable("return artifact not found error",
+				func(filter weles.ArtifactFilter, expected ...weles.ArtifactInfo) {
+					_, _, err := goldenUnicorn.Filter(filter, defaultSorter, emptyPaginator)
+					Expect(err).To(Equal(weles.ErrArtifactNotFound))
+				},
+				Entry("filter JobID not in db", noJobFilter),
+				Entry("filter Type not in db", noTypeFilter),
+				Entry("filter Status not in db", noStatusFilter),
+				Entry("filter Alias not in db", noAliasFilter),
+				Entry("no artifact in db matches filter", noMatchFilter),
+			)
 		})
-		DescribeTable("artifact status change",
-			func(change weles.ArtifactStatusChange, expectedErr error) {
-
-				var a weles.ArtifactInfo
-				err := goldenUnicorn.SetStatus(change)
-				if expectedErr == nil {
+		Describe("Sorting", func() {
+			BeforeEach(func() {
+				trans, err := goldenUnicorn.dbmap.Begin()
+				Expect(err).ToNot(HaveOccurred())
+				defer trans.Commit()
+				for _, a := range testArtifacts {
+					err := trans.Insert(&a)
 					Expect(err).ToNot(HaveOccurred())
-					a, err = goldenUnicorn.SelectPath(change.Path)
-					Expect(err).ToNot(HaveOccurred())
-					Expect(a.Status).To(Equal(change.NewStatus))
-				} else {
-					Expect(err).To(Equal(expectedErr))
-					a, err = goldenUnicorn.SelectPath(change.Path)
-					Expect(err).To(HaveOccurred())
-					Expect(a).To(Equal(weles.ArtifactInfo{}))
 				}
-			},
-			Entry("change status of artifact not present in ArtifactDB",
-				weles.ArtifactStatusChange{
-					Path:      invalidPath,
-					NewStatus: weles.ArtifactStatusDOWNLOADING,
+			})
+			DescribeTable("Should return correctly sorted artifacts",
+				func(sorter weles.ArtifactSorter) {
+					result, _, err := goldenUnicorn.Filter(emptyFilter, sorter, emptyPaginator)
+					Expect(err).ToNot(HaveOccurred())
+					var currID int
+					if sorter.SortOrder == weles.SortOrderAscending {
+						for _, a := range result {
+							if currID == 0 {
+								currID = int(a.ID)
+								continue
+							}
+							Expect(a.ID).To(BeEquivalentTo(currID + 1))
+							currID = int(a.ID)
+						}
+					} else {
+						for _, a := range result {
+							if currID == 0 {
+								currID = int(a.ID)
+								continue
+							}
+							Expect(a.ID).To(BeEquivalentTo(currID - 1))
+							currID = int(a.ID)
+						}
+					}
 				},
-				sql.ErrNoRows),
-			Entry("change status of artifact present in ArtifactDB",
-				weles.ArtifactStatusChange{
-					Path:      artifact.Path,
-					NewStatus: weles.ArtifactStatusDOWNLOADING,
+				Entry("By ID, Ascending", ascendingSorter),
+				Entry("By ID, Descending", descendingSorter),
+			)
+
+		})
+	})
+	Describe("Pagination", func() {
+		Context("Database is filled with generatedRecordsCount records", func() {
+			DescribeTable("paginating through artifacts",
+				func(paginator weles.ArtifactPagination,
+					expectedResponseLength, expectedRemainingRecords int) {
+
+					result, list, err := silverHoneybadger.Filter(emptyFilter, defaultSorter, paginator)
+					Expect(err).ToNot(HaveOccurred())
+
+					Expect(len(result)).To(BeEquivalentTo(expectedResponseLength))
+					Expect(list.TotalRecords).To(BeEquivalentTo(generatedRecordsCount))
+					Expect(list.RemainingRecords).To(BeEquivalentTo(expectedRemainingRecords))
 				},
-				nil),
-		)
+				// please keep in mind that data is sorted in descending order.
+				Entry("first and last page (limit is 0)",
+					weles.ArtifactPagination{ID: 0, Limit: 0, Forward: true},
+					generatedRecordsCount, 0),
+
+				Entry("first page, paginating forward",
+					weles.ArtifactPagination{ID: 0, Limit: pageLimit, Forward: true},
+					pageLimit, (generatedRecordsCount-pageLimit)),
+
+				Entry("second page, paginating forward",
+					weles.ArtifactPagination{
+						ID:      (generatedRecordsCount - pageLimit + 1),
+						Limit:   pageLimit,
+						Forward: true,
+					},
+					pageLimit,
+					(generatedRecordsCount-(2*pageLimit))),
+
+				Entry("last page, paginating forward",
+					weles.ArtifactPagination{
+						ID: int64(generatedRecordsCount -
+							(pageLimit * int(generatedRecordsCount/pageLimit)) + 1),
+						Limit:   pageLimit,
+						Forward: true,
+					},
+					generatedRecordsCount-(pageLimit*(pageCount-1)),
+					0),
+
+				Entry("second to last page, paginating backward",
+					weles.ArtifactPagination{
+						ID: int64(generatedRecordsCount -
+							(pageLimit * int(generatedRecordsCount/pageLimit))),
+						Limit:   pageLimit,
+						Forward: false,
+					},
+					pageLimit,
+					pageLimit*(pageCount-2)),
+
+				Entry("first page, paginating backward",
+					weles.ArtifactPagination{
+						ID:      int64(generatedRecordsCount - pageLimit),
+						Limit:   pageLimit,
+						Forward: false,
+					},
+					pageLimit,
+					0),
+			)
+		})
 	})
 })
