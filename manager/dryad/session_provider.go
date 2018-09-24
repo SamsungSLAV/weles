136c2010a7aae018668575aefd0543a7bb929ff4
Alexander Mazuruk <a.mazuruk@samsung.com>
2018-09-24 13:49:58 +0200

    Pipe session provider std{out,err} to os.stderr
    This should be integrated with logger somehow- e.g. shown as Debug logs
    with some prefix.
    
    Change-Id: Ieb4f65d362ed9e63f1efd1c87826a4cd01687c43
    Signed-off-by: Alexander Mazuruk <a.mazuruk@samsung.com>

--- manager/dryad/session_provider.go
+++ manager/dryad/session_provider.go
@@ -21,6 +21,8 @@ import (
 	"context"
 	"crypto/rsa"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 	"time"
 
@@ -107,8 +109,8 @@ func (d *sessionProvider) executeRemoteCommand(cmd string) ([]byte, []byte, erro
 	}()
 
 	var stdout, stderr bytes.Buffer
-	session.Stdout = &stdout
-	session.Stderr = &stderr
+	session.Stdout = io.MultiWriter(&stdout, os.Stderr)
+	session.Stderr = io.MultiWriter(&stderr, os.Stderr)
 
 	err = session.Run(cmd)
 	return stdout.Bytes(), stderr.Bytes(), err
