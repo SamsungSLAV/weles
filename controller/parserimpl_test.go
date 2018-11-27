f8f1b769bfa459a427954ba6c7e9401caa302c9c
Pawel Wieczorek <p.wieczorek2@samsung.com>
2018-11-27 13:41:07 +0100

    [WIP] Intercept error logs from controller pkg tests (Parser)
    ----------------8<-------------[ cut here ]------------------
    
    Following note will be removed once WIP label is dropped.
    
    Author is unsure if this approach for log interception is correct, hence
    WIP publication. ParserImpl was the shortest to modify:
    
    * 27 Boruter
    * 17 Downloader
    * 15 Dryader
    * 79 JobsController
    * 8  Parser
    
    (number of TCs per Controller module).
    
    ----------------8<-------------[ cut here ]------------------
    
    Change-Id: Ica0d5dc4a37158322e2e142b8d0a51db703a2d4b
    Signed-off-by: Pawel Wieczorek <p.wieczorek2@samsung.com>

--- controller/parserimpl_test.go
+++ controller/parserimpl_test.go
@@ -18,6 +18,9 @@ package controller
 
 import (
 	"errors"
+	"sync"
+
+	"github.com/SamsungSLAV/perun/testutil"
 
 	"github.com/SamsungSLAV/weles"
 	cmock "github.com/SamsungSLAV/weles/controller/mock"
@@ -35,6 +38,7 @@ var _ = Describe("ParserImpl", func() {
 	var yp *mock.MockParser
 	var h Parser
 	var ctrl *gomock.Controller
+	lock := sync.Locker(new(sync.Mutex))
 	j := weles.JobID(0xCAFE)
 	goodpath := weles.ArtifactPath("/tmp/weles_test")
 	badpath := weles.ArtifactPath("/such/path/does/not/exist")
@@ -43,6 +47,9 @@ var _ = Describe("ParserImpl", func() {
 	err := errors.New("test error")
 
 	BeforeEach(func() {
+		lock.Lock()
+		defer lock.Unlock()
+
 		ctrl = gomock.NewController(GinkgoT())
 
 		jc = cmock.NewMockJobsController(ctrl)
@@ -53,6 +60,9 @@ var _ = Describe("ParserImpl", func() {
 		r = h.Listen()
 	})
 	AfterEach(func() {
+		lock.Lock()
+		defer lock.Unlock()
+
 		ctrl.Finish()
 	})
 	Describe("NewParser", func() {
@@ -85,6 +95,7 @@ var _ = Describe("ParserImpl", func() {
 			})))
 		})
 		It("should fail when unable to set config", func() {
+			lock.Lock()
 			gomock.InOrder(
 				jc.EXPECT().SetStatusAndInfo(j, weles.JobStatusPARSING, ""),
 				jc.EXPECT().GetYaml(j).Return(yaml, nil),
@@ -96,17 +107,26 @@ var _ = Describe("ParserImpl", func() {
 				yp.EXPECT().ParseYaml(yaml).Return(&config, nil),
 				jc.EXPECT().SetConfig(j, config).Return(err),
 			)
+			lock.Unlock()
 
-			h.Parse(j)
+			log, logerr := testutil.WithStderrMocked(func() {
+				defer GinkgoRecover()
+				lock.Lock()
+				defer lock.Unlock()
+				h.Parse(j)
 
-			expectedNotification := notifier.Notification{
-				JobID: j,
-				OK:    false,
-				Msg:   "Internal Weles error while setting config : " + err.Error(),
-			}
-			Eventually(r).Should(Receive(Equal(expectedNotification)))
+				expectedNotification := notifier.Notification{
+					JobID: j,
+					OK:    false,
+					Msg:   "Internal Weles error while setting config : " + err.Error(),
+				}
+				Eventually(r).Should(Receive(Equal(expectedNotification)))
+			})
+			Expect(logerr).NotTo(HaveOccurred())
+			Expect(log).To(ContainSubstring("Failed to set config for Job."))
 		})
 		It("should fail when unable to parse yaml", func() {
+			lock.Lock()
 			gomock.InOrder(
 				jc.EXPECT().SetStatusAndInfo(j, weles.JobStatusPARSING, ""),
 				jc.EXPECT().GetYaml(j).Return(yaml, nil),
@@ -117,17 +137,26 @@ var _ = Describe("ParserImpl", func() {
 					}).Return(goodpath, nil),
 				yp.EXPECT().ParseYaml(yaml).Return(&weles.Config{}, err),
 			)
+			lock.Unlock()
 
-			h.Parse(j)
+			log, logerr := testutil.WithStderrMocked(func() {
+				defer GinkgoRecover()
+				lock.Lock()
+				defer lock.Unlock()
+				h.Parse(j)
 
-			expectedNotification := notifier.Notification{
-				JobID: j,
-				OK:    false,
-				Msg:   "Error parsing yaml file : " + err.Error(),
-			}
-			Eventually(r).Should(Receive(Equal(expectedNotification)))
+				expectedNotification := notifier.Notification{
+					JobID: j,
+					OK:    false,
+					Msg:   "Error parsing yaml file : " + err.Error(),
+				}
+				Eventually(r).Should(Receive(Equal(expectedNotification)))
+			})
+			Expect(logerr).NotTo(HaveOccurred())
+			Expect(log).To(ContainSubstring("Failed to parse Job description for job."))
 		})
 		It("should fail when unable to write yaml file", func() {
+			lock.Lock()
 			gomock.InOrder(
 				jc.EXPECT().SetStatusAndInfo(j, weles.JobStatusPARSING, ""),
 				jc.EXPECT().GetYaml(j).Return(yaml, nil),
@@ -137,18 +166,27 @@ var _ = Describe("ParserImpl", func() {
 						Type:  weles.ArtifactTypeYAML,
 					}).Return(badpath, nil),
 			)
+			lock.Unlock()
 
-			h.Parse(j)
+			log, logerr := testutil.WithStderrMocked(func() {
+				defer GinkgoRecover()
+				lock.Lock()
+				defer lock.Unlock()
+				h.Parse(j)
 
-			expectedNotification := notifier.Notification{
-				JobID: j,
-				OK:    false,
-				Msg: "Internal Weles error while saving file in ArtifactDB : " +
-					"open " + string(badpath) + ": no such file or directory",
-			}
-			Eventually(r).Should(Receive(Equal(expectedNotification)))
+				expectedNotification := notifier.Notification{
+					JobID: j,
+					OK:    false,
+					Msg: "Internal Weles error while saving file in ArtifactDB : " +
+						"open " + string(badpath) + ": no such file or directory",
+				}
+				Eventually(r).Should(Receive(Equal(expectedNotification)))
+			})
+			Expect(logerr).NotTo(HaveOccurred())
+			Expect(log).To(ContainSubstring("Failed to write Job description to file."))
 		})
 		It("should fail when unable to create path in ArtifactDB", func() {
+			lock.Lock()
 			gomock.InOrder(
 				jc.EXPECT().SetStatusAndInfo(j, weles.JobStatusPARSING, ""),
 				jc.EXPECT().GetYaml(j).Return(yaml, nil),
@@ -158,43 +196,69 @@ var _ = Describe("ParserImpl", func() {
 						Type:  weles.ArtifactTypeYAML,
 					}).Return(weles.ArtifactPath(""), err),
 			)
+			lock.Unlock()
 
-			h.Parse(j)
+			log, logerr := testutil.WithStderrMocked(func() {
+				defer GinkgoRecover()
+				lock.Lock()
+				defer lock.Unlock()
+				h.Parse(j)
 
-			expectedNotification := notifier.Notification{
-				JobID: j,
-				OK:    false,
-				Msg: "Internal Weles error while creating file path in ArtifactDB : " +
-					err.Error(),
-			}
-			Eventually(r).Should(Receive(Equal(expectedNotification)))
+				expectedNotification := notifier.Notification{
+					JobID: j,
+					OK:    false,
+					Msg: "Internal Weles error while creating file path in ArtifactDB : " +
+						err.Error(),
+				}
+				Eventually(r).Should(Receive(Equal(expectedNotification)))
+			})
+			Expect(logerr).NotTo(HaveOccurred())
+			Expect(log).To(ContainSubstring("Failed to create Job description."))
 		})
 		It("should fail when unable to get yaml", func() {
+			lock.Lock()
 			gomock.InOrder(
 				jc.EXPECT().SetStatusAndInfo(j, weles.JobStatusPARSING, ""),
 				jc.EXPECT().GetYaml(j).Return([]byte{}, err),
 			)
+			lock.Unlock()
 
-			h.Parse(j)
+			log, logerr := testutil.WithStderrMocked(func() {
+				defer GinkgoRecover()
+				lock.Lock()
+				defer lock.Unlock()
+				h.Parse(j)
 
-			expectedNotification := notifier.Notification{
-				JobID: j,
-				OK:    false,
-				Msg:   "Internal Weles error while getting yaml description : " + err.Error(),
-			}
-			Eventually(r).Should(Receive(Equal(expectedNotification)))
+				expectedNotification := notifier.Notification{
+					JobID: j,
+					OK:    false,
+					Msg:   "Internal Weles error while getting yaml description : " + err.Error(),
+				}
+				Eventually(r).Should(Receive(Equal(expectedNotification)))
+			})
+			Expect(logerr).NotTo(HaveOccurred())
+			Expect(log).To(ContainSubstring("Failed to get Job description."))
 		})
 		It("should fail when unable to change job status", func() {
+			lock.Lock()
 			jc.EXPECT().SetStatusAndInfo(j, weles.JobStatusPARSING, "").Return(err)
+			lock.Unlock()
 
-			h.Parse(j)
+			log, logerr := testutil.WithStderrMocked(func() {
+				defer GinkgoRecover()
+				lock.Lock()
+				defer lock.Unlock()
+				h.Parse(j)
 
-			expectedNotification := notifier.Notification{
-				JobID: j,
-				OK:    false,
-				Msg:   "Internal Weles error while changing Job status : " + err.Error(),
-			}
-			Eventually(r).Should(Receive(Equal(expectedNotification)))
+				expectedNotification := notifier.Notification{
+					JobID: j,
+					OK:    false,
+					Msg:   "Internal Weles error while changing Job status : " + err.Error(),
+				}
+				Eventually(r).Should(Receive(Equal(expectedNotification)))
+			})
+			Expect(logerr).NotTo(HaveOccurred())
+			Expect(log).To(ContainSubstring("Failed to set JobStatus to PARSING."))
 		})
 	})
 })
