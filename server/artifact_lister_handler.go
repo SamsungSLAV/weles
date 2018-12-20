9f65799469a84c519c9ce044ed19e50628fe0e23
Alexander Mazuruk <a.mazuruk@samsung.com>
2018-12-20 13:16:47 +0100

    Fix server panic - request for page after last obj
    TODO: add tests
    
    Change-Id: I8c705a34e2d9544882e5fc36503a9e418ea50dc9
    Signed-off-by: Alexander Mazuruk <a.mazuruk@samsung.com>

--- server/artifact_lister_handler.go
+++ server/artifact_lister_handler.go
@@ -99,16 +99,20 @@ func responderArtifact200(listInfo weles.ListInfo, paginator weles.ArtifactPagin
 		// keep in mind that ArtifactPath in paginator is taken from query parameter,
 		// not ArtifactManager
 		if paginator.Forward {
-			tmp := artifactInfoReturned[0].ID
-			artifactListerURL.Before = &tmp
+			if len(artifactInfoReturned) != 0 {
+				tmp := artifactInfoReturned[0].ID
+				artifactListerURL.Before = &tmp
+			}
 			if defaultPageLimit != paginator.Limit {
 				tmp := paginator.Limit
 				artifactListerURL.Limit = &tmp
 			}
 			responder.SetPrevious(artifactListerURL.String())
 		} else {
-			tmp := artifactInfoReturned[len(artifactInfoReturned)-1].ID
-			artifactListerURL.After = &tmp
+			if len(artifactInfoReturned) != 0 {
+				tmp := artifactInfoReturned[len(artifactInfoReturned)-1].ID
+				artifactListerURL.After = &tmp
+			}
 			if defaultPageLimit != paginator.Limit {
 				tmp2 := paginator.Limit
 				artifactListerURL.Limit = &tmp2
