390c905bfa9c443dea045f5f5b9aafe1890cd336
Alexander Mazuruk <a.mazuruk@samsung.com>
2018-12-28 18:09:52 +0100

    Rename *Pagination to *Paginator for consistency
    ... with *Sorter and *Filter objects.
    
    Change-Id: I61f340acf8c7c742525bb8717c1220f443c15b71
    Signed-off-by: Alexander Mazuruk <a.mazuruk@samsung.com>

--- server/artifact_lister_handler_test.go
+++ server/artifact_lister_handler_test.go
@@ -47,7 +47,7 @@ var _ = Describe("Listing artifacts with server initialized", func() {
 
 	type queryPaginator struct {
 		query     string
-		paginator weles.ArtifactPagination
+		paginator weles.ArtifactPaginator
 	}
 	// data to test against
 	var (
@@ -94,9 +94,9 @@ var _ = Describe("Listing artifacts with server initialized", func() {
 		sorterDefault = sorterAscID
 
 		// when pagination is on and no query params are set. When used, limit should also be set.
-		emptyPaginatorOn = weles.ArtifactPagination{Forward: true}
+		emptyPaginatorOn = weles.ArtifactPaginator{Forward: true}
 		// when pagination is off
-		emptyPaginatorOff = weles.ArtifactPagination{}
+		emptyPaginatorOff = weles.ArtifactPaginator{}
 
 		artifactInfo420 = fixtures.CreateArtifactInfoSlice(420)
 	)
@@ -324,7 +324,7 @@ var _ = Describe("Listing artifacts with server initialized", func() {
 					TotalRecords:     uint64(len(artifactInfo420)),
 					RemainingRecords: 0,
 				}
-				var paginator weles.ArtifactPagination
+				var paginator weles.ArtifactPaginator
 				if pageLimit == 0 {
 					paginator = emptyPaginatorOff
 				} else {
@@ -367,7 +367,7 @@ var _ = Describe("Listing artifacts with server initialized", func() {
 	Describe("Pagination turned on", func() {
 		Describe("Correct request", func() {
 			DescribeTable("server should set paginator object depending on query params",
-				func(query string, expectedPaginator weles.ArtifactPagination) {
+				func(query string, expectedPaginator weles.ArtifactPaginator) {
 					apiDefaults.PageLimit = 500
 
 					listInfo := weles.ListInfo{
@@ -387,17 +387,17 @@ var _ = Describe("Listing artifacts with server initialized", func() {
 
 				},
 				Entry("when no query params set", "",
-					weles.ArtifactPagination{Forward: true, Limit: 500}),
+					weles.ArtifactPaginator{Forward: true, Limit: 500}),
 				Entry("when after param is set", "?after=30",
-					weles.ArtifactPagination{Forward: true, Limit: 500, ID: 30}),
+					weles.ArtifactPaginator{Forward: true, Limit: 500, ID: 30}),
 				Entry("when after and limit params are set", "?after=30&limit=20",
-					weles.ArtifactPagination{Forward: true, Limit: 20, ID: 30}),
+					weles.ArtifactPaginator{Forward: true, Limit: 20, ID: 30}),
 				Entry("when before param is set", "?before=30",
-					weles.ArtifactPagination{Forward: false, Limit: 500, ID: 30}),
+					weles.ArtifactPaginator{Forward: false, Limit: 500, ID: 30}),
 				Entry("when before and limit params are set", "?before=30&limit=15",
-					weles.ArtifactPagination{Forward: false, Limit: 15, ID: 30}),
+					weles.ArtifactPaginator{Forward: false, Limit: 15, ID: 30}),
 				Entry("when limit param is set", "?limit=30",
-					weles.ArtifactPagination{Forward: true, Limit: 30}),
+					weles.ArtifactPaginator{Forward: true, Limit: 30}),
 			)
 
 			DescribeTable("server should respond with 200/206 depending on "+
